5b42f60c3ecf0820412b9bc419caad4e9adcb0a3
Wizardy <wizardhassan99@gmail.com>
2024-03-22 23:41:32 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Forms from './01_Exercice/Forms'
+import ThemeContext from './02_Exercice/ThemeContext'
 import './App.css'
 
 function App() {
 
   return (
     <div className="app">
-      <Forms/>
+      {/* <Forms/> */}
+      <ThemeContext/>
     </div>
   )
 }
